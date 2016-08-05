8ccc064f620ba971cea8e2a2967524f8afa0d2c3
Erith Margolis <dischordiandesigns@gmail.com>
2016-08-05 15:19:32 -0700

    Finished
    Resizable grid and reset are done

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
+
+
 for (var x =0; x<16;x++)
 {
         for(var y = 0; y < 16; y++) {
-        	var $cell = $('<div class="cell"></div>');
+        	 var $cell = $('<div class="cell"></div>');
         	 $cell.appendTo('.container');
         }
 }
+
+$(".reset").click(function() {
+  $('.cell').css("background-color","white");
+});
+$(".sizeChange").click(function() {
+	var cellCount = prompt("Please enter cellCount", "4");
+	if( cellCount > 0)
+		{
+			var newSize = 12 * cellCount;
+			$('.container').height(newSize);
+			$('.container').width(newSize);
+			$('.cell').remove();
+			for (var x =0; x<cellCount;x++)
+			{
+		        for(var y = 0; y < cellCount; y++) {
+		        	 var $cell = $('<div class="cell"></div>');
+		        	 $cell.appendTo('.container');
+		        }
+			}
+		}
+});
+})
+$(document).on('mouseenter', '.cell', function(ev){
+   $(this).css("background-color","black");
 })
